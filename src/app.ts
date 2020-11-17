664e8ecc93cd82242e6f7d19f8de05def2d24fc0
artem_vasiliev <vincere802@gmail.com>
2020-11-17 16:16:01 +0200

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,63 @@
+import express from "express";
+import http from "http";
+import socketIO from 'socket.io'
+import {type} from "os";
+
+
+const app = express();
+const server = http.createServer(app);
+const socket = socketIO(server);
+
+
+app.get('/', (req, res) => {
+    res.send("Hello it's WS SERVER")
+});
+
+
+const messages:Array<any> = [];
+
+
+const usersState = new Map();
+
+socket.on("connection", (socketChannel) => {
+
+    usersState.set(socketChannel, {id: new Date().getTime().toString(), name: 'anonym'});
+
+    socket.on('disconnect', () => {
+        usersState.delete(socketChannel)
+    });
+
+    socketChannel.on('client-name-sent', (name: string) => {
+
+        if(typeof name !== 'string' || name === '') {
+            return;
+        }
+
+        const user = usersState.get(socketChannel);
+        user.name = name;
+    });
+
+    socketChannel.on('client-message-sent', (message: string) => {
+        if (typeof message !== 'string') {
+            return;
+        }
+
+        const user = usersState.get(socketChannel);
+
+        let messageItem = {
+            id: new Date().getTime(),
+            message: message,
+            user: {id: user.id, name: user.name}
+        };
+        messages.push(messageItem);
+
+        socket.emit('new-message-sent', messageItem);
+    });
+
+    socketChannel.emit('init-messages-published', messages);
+
+    console.log("User Connected!");
+});
+
+
+server.listen(3005);
